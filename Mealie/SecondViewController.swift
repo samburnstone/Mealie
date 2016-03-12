ceb753404fb9d245b81a699f54f6635c1e765201
Sam Burnstone <sam.burnstone@gmail.com>
2016-03-12 18:08:43 +0000

    Initial Commit

---
+++ Mealie/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mealie
+//
+//  Created by Sam Burnstone on 12/03/2016.
+//  Copyright © 2016 Sam Burnstone. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
