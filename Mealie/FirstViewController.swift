7feebe4b0fe5037f6713ed0acdcc9455d4587873
Sam Burnstone <sam.burnstone@gmail.com>
2016-03-14 21:35:06 +0000

    Adds base tableview to view controller

--- Mealie/FirstViewController.swift
+++ Mealie/FirstViewController.swift
@@ -15,11 +15,29 @@ class FirstViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension FirstViewController: UITableViewDataSource
+{
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("RecipeCell")!
+        
+        cell.textLabel?.text = "Cell \(indexPath.row)"
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 20
+    }
+}
+
+extension FirstViewController: UITableViewDelegate
+{
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        return 80
+    }
+}
