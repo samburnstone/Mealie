e70c12eeb90b0a90fc44a95051ec6fc5bec3da45
Sam Burnstone <sam.burnstone@gmail.com>
2016-03-19 19:39:55 +0000

    Adds recipe and recipe provider structs
    The provider simply returns an array of Recipe structs for the time
    being. Obviously in the future, these will be retrieved from disk/
    cloud storage.

---
+++ Mealie/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  Mealie
+//
+//  Created by Sam Burnstone on 15/03/2016.
+//  Copyright © 2016 Sam Burnstone. All rights reserved.
+//
+
+import UIKit
+
+struct Recipe
+{
+    let title: String
+    let image: UIImage?
+}
